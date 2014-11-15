af00b4e07df1b146f069c0d02a09db6c3021232f
quietcreep <dustinchaffin@gmail.com>
2014-11-15 10:49:22 -0700

    Ugh

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "quietcreep:formation",
   summary: "Slender form creation w/ client & server side validation",
-  version: "1.0.3_3",
+  version: "1.0.3_4",
   git: "http://github.com/quietcreep/meteor-formation"
 });
 
@@ -11,8 +11,8 @@ Package.onUse( function( api ) {
 
   var both = [ 'client', 'server' ];
 
-  api.use([ 'quietcreep:formation-core@1.0.3_3', 'quietcreep:formation-blaze-widgets@1.0.3' ]);
-  api.imply([ 'quietcreep:formation-core@1.0.3_3', 'quietcreep:formation-blaze-widgets@1.0.3' ]);
+  api.use([ 'quietcreep:formation-core@1.0.3_4', 'quietcreep:formation-blaze-widgets@1.0.3' ]);
+  api.imply([ 'quietcreep:formation-core@1.0.3_4', 'quietcreep:formation-blaze-widgets@1.0.3' ]);
 });
 
 
